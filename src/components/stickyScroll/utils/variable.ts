cd0f3838aea244f90618c136150f36e3b6dcca37
searchme86 <searchme86@gmail.com>
2024-12-31 00:27:02 +0900

    ♻️ create a query dom function

--- src/components/stickyScroll/utils/variable.ts
+++ src/components/stickyScroll/utils/variable.ts
@@ -1,6 +1,16 @@
-const listMenu = document.querySelector('.list_menu');
-const listItems = document.querySelectorAll('.list-item_menu .text_menu');
-const menuContents = document.querySelectorAll('.menu_content');
-const header = document.querySelector('.header');
+const queryElement = <T extends HTMLElement>(selector: string): T | null => {
+  return document.querySelector(selector);
+};
+
+const queryElements = <T extends HTMLElement>(
+  selector: string
+): NodeListOf<T> | null => {
+  return document.querySelectorAll(selector);
+};
+
+const listMenu = queryElement<HTMLDivElement>('.list_menu');
+const listItems = queryElements<HTMLDivElement>('.list-item_menu .text_menu');
+const menuContents = queryElements<HTMLDivElement>('.menu_content');
+const header = queryElement<HTMLDivElement>('.header');
 
 export { listMenu, listItems, menuContents, header };
