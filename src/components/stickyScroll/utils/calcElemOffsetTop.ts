310f17004265ada174c184f42a8014e8a2235a2b
searchme86 <searchme86@gmail.com>
2025-01-02 11:18:32 +0900

    ♻️ offsetTop을 구할 수 있는지 util기능 추가

--- src/components/stickyScroll/utils/calcElemOffsetTop.ts
+++ src/components/stickyScroll/utils/calcElemOffsetTop.ts
@@ -1,19 +1,37 @@
-// 클래스명으로 검색한 엘리먼트가 offsetTop을 가지고 있는지를 체크하는 함수입니다.
-const checkOffsetTopOrNot = (element: string) => {
-  const foundDom = document.querySelector(element) as HTMLElement | null;
-  if (foundDom) {
-    if (foundDom.offsetTop === 0) {
-      return;
+// 클래스명으로 검색한 엘리먼트로 offsetTop을 구할 수 있는지 없는지를 체크하는 함수
+const checkIfElemHasOffsetTopOrNot = (element: string) => {
+  const searchedDom = document.querySelector(element) as HTMLElement | null;
+
+  if (searchedDom) {
+    const domOffsetParent = searchedDom.offsetParent;
+
+    if (domOffsetParent) {
+      const domPositionStyle =
+        window.getComputedStyle(domOffsetParent).position;
+
+      if (
+        domOffsetParent === document.body ||
+        searchedDom.offsetTop === 0 ||
+        domPositionStyle === 'static'
+      ) {
+        return { type: 'false', offsetValue: 0 };
+      }
+      return { type: 'true', offsetValue: searchedDom };
     }
-    return foundDom;
   }
 };
 
+// 엘리먼트의 클래스명을 입력하여 해당 돔의 offsetTop을 구하는 함수
+
 const calcElemOffsetTopValue = (element: string, value = 0) => {
-  if (element) {
-    const offsetAvailElem = checkOffsetTopOrNot(element);
-    if (offsetAvailElem) {
-      return offsetAvailElem.offsetTop - value;
+  const something = checkIfElemHasOffsetTopOrNot(element);
+  if (something) {
+    const { type, offsetValue } = something;
+
+    if (type === 'true' && offsetValue instanceof HTMLElement) {
+      return offsetValue.offsetTop - value;
+    } else {
+      return 0;
     }
   }
 };
