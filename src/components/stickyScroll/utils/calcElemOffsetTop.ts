bfc5c7e9d533a1d08fa579303c3f3c740b6a6f83
searchme86 <searchme86@gmail.com>
2025-01-03 14:32:39 +0900

    ♻️ offsetTop구하는 함수 디버깅 메세지 만드는 코드 추가

--- src/components/stickyScroll/utils/calcElemOffsetTop.ts
+++ src/components/stickyScroll/utils/calcElemOffsetTop.ts
@@ -1,37 +1,72 @@
+const getElemPositionType = (element: Element | null) => {
+  if (element) {
+    const elemPositionType = window.getComputedStyle(element).position;
+    return elemPositionType;
+  }
+};
+
 // 클래스명으로 검색한 엘리먼트로 offsetTop을 구할 수 있는지 없는지를 체크하는 함수
 const checkIfElemHasOffsetTopOrNot = (element: singleClassType) => {
   const searchedDom = document.querySelector(element) as HTMLElement | null;
 
-  if (searchedDom) {
-    const domOffsetParent = searchedDom?.offsetParent;
-    console.log('0. searchedDom', searchedDom);
-    console.log('1.domOffsetParent', domOffsetParent);
+  try {
+    if (searchedDom) {
+      const domOffsetParent = searchedDom?.offsetParent;
 
-    const domInfo = {
-      tagName: searchedDom?.tagName.toLowerCase(),
-      className: searchedDom?.className,
-      parentClassName: domOffsetParent?.className,
-    };
+      if (!domOffsetParent || searchedDom.style.position === 'fixed') {
+        throw new Error(
+          '함수 checkIfElemHasOffsetTopOrNot : searchedDom의 position이 fixed 속성이거나 domOffsetParent이 존재하지 않습니다'
+        );
+      }
 
-    if (domOffsetParent) {
-      const domPositionStyle =
-        window.getComputedStyle(domOffsetParent).position;
+      const domInfo: {
+        searchedDom: {
+          searchedDomTagName: string | undefined;
+          searchedDomOffsetParent: Element | null;
+          searchedDomPosition: string | number | undefined;
+        };
+        offsetParent: {
+          offsetParentClassName: string | undefined;
+          offsetParentPosition: string | number | undefined;
+        };
+      } = {
+        searchedDom: {
+          searchedDomTagName: searchedDom?.tagName.toLowerCase(),
+          searchedDomOffsetParent: searchedDom?.offsetParent,
+          searchedDomPosition: getElemPositionType(searchedDom),
+        },
+        offsetParent: {
+          offsetParentClassName: domOffsetParent?.className,
+          offsetParentPosition: getElemPositionType(domOffsetParent),
+        },
+      };
 
-      if (
-        domOffsetParent === document.body ||
-        searchedDom.offsetTop === 0 ||
-        domPositionStyle === 'static'
-      ) {
+      const {
+        searchedDom: {
+          searchedDomTagName,
+          searchedDomOffsetParent,
+          searchedDomPosition,
+        },
+        offsetParent: { offsetParentClassName, offsetParentPosition },
+      } = domInfo;
+
+      if (domOffsetParent) {
         return {
-          type: 'false',
-          offsetValue: 0,
-          message: `[에러] : offsetTop 계산 : 클래스 ${domInfo['className']}, ${domInfo['tagName']} 엘리먼트의 offsetParent의 css position 속성을 체크해보세요`,
+          type: 'true',
+          offsetValue: searchedDom,
+          message: `[정상] : 함수인자로 전달한 엘리먼트, 클래스 ${searchedDomTagName} ${searchedDom}의 position은 ${searchedDomPosition}이며, offsetParent는 ${searchedDomOffsetParent}입니다. offsetParent은 클래스 ${offsetParentClassName}이며, position은 ${offsetParentPosition}입니다`,
         };
       }
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      console.log('error');
+      console.error(e.message);
       return {
-        type: 'true',
-        offsetValue: searchedDom,
-        message: `[정상] : offsetTop 계산 : 클래스 ${domInfo['className']}, ${domInfo['tagName']} 엘리먼트의 offsetParent는 클래스 .${domInfo['parentClassName']}로, 계산값으로 사용 가능합니다`,
+        type: 'false',
+        offsetValue: 0,
+        message:
+          '[에러]: 인자로 전달한 DOMElem의 position이 fixed 속성인지 확인해주세요',
       };
     }
   }
